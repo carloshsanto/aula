c39e37b7b7ca6404c67bd0cc71c7965529d967cd
Carlos Henrique dos Santos <71515378+carloshsanto@users.noreply.github.com>
2021-11-06 09:40:57 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TabMenuModule} from 'primeng/tabmenu';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabMenuModule,    
+        
   ],
   providers: [],
   bootstrap: [AppComponent]
